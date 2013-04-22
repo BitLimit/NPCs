0c2405384bafc10fa042c918df58a485c7dfb414
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-04-21 19:13:07 -0700

    Comment 'dat code.

--- src/main/java/com/kolinkrewinkel/BitLimitNPCs/BlacksmithInteractBehavior.java
+++ src/main/java/com/kolinkrewinkel/BitLimitNPCs/BlacksmithInteractBehavior.java
@@ -94,7 +94,7 @@ public class BlacksmithInteractBehavior extends InteractBehavior {
 
         // Capture the NPC's location and grab the closest anvil.
         Location npcLocation = npc.getLocation();
-        Location closest = closestLocation(npcLocation, Material.ANVIL);
+        Location destination = closestLocation(npcLocation, Material.ANVIL);
 
         // Disable the NPC gazing around.
         DesireLookRandomly desireLookRandomly = behaviorEntity.getMind().getMovementDesire(DesireLookRandomly.class);
@@ -103,7 +103,6 @@ public class BlacksmithInteractBehavior extends InteractBehavior {
         DesireLookAtNearest desireLookAtNearest = behaviorEntity.getMind().getMovementDesire(DesireLookAtNearest.class);
         desireLookAtNearest.setLookPossibility(-1F);
 
-        Location destination = closest;
         behaviorEntity.move(destination);
         behaviorEntity.lookAt(destination);
         double distanceAway = npcLocation.distance(destination);
@@ -111,55 +110,55 @@ public class BlacksmithInteractBehavior extends InteractBehavior {
         class FinishRepairTrip implements Runnable {
             private final Player npc;
             private final Location gaze;
-            private final Player toLookAt;
 
-            public FinishRepairTrip(Player npc, Location gaze, Player toLookAt) {
+            public FinishRepairTrip(Player npc, Location gaze) {
                 this.npc = npc;
                 this.gaze = gaze;
-                this.toLookAt = toLookAt;
             }
 
             public void run() {
 
-                if (!this.gaze.equals(this.npc.getLocation()))
+                if (!this.gaze.equals(this.npc.getLocation())) // Pretend he's working at the anvil.
                     npc.setSneaking(true);
 
                 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
                     public void run() {
-                        npc.setSneaking(false);
-
-                        npc.getWorld().playSound(npc.getLocation(), Sound.ANVIL_USE, 1F, 1F);
-                        RemoteEntity entity = RemoteEntities.getRemoteEntityFromEntity(npc);
-
-                        entity.lookAt(toLookAt);
+                        npc.setSneaking(false); // Return to standing.
+                        npc.getWorld().playSound(npc.getLocation(), Sound.ANVIL_USE, 1F, 1F); // Simulate a repair. :P
                     }
                 }, 20L);
             }
         }
 
-        Long wait = Math.round((distanceAway / 4) * 20);
-        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new FinishRepairTrip(npc, destination, inPlayer), wait);
+        Long wait = Math.round((distanceAway / 4) * 20); // Calculates the time needed to wait for the next operation (this is all async, so the delay isn't pre-factored.
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new FinishRepairTrip(npc, destination), wait);
 
-        short zero = 0;
-        repairItem.setDurability(zero);
+        // Actually repair the item.
+        repairItem.setDurability((short)0);
 
         class ReturnNPC implements Runnable {
             private final Location returnLocation;
             private final RemotePlayer entity;
+            private final Player toLookAt;
 
-            public ReturnNPC(Location returnLocation, RemotePlayer entity) {
+            public ReturnNPC(Location returnLocation, RemotePlayer entity, Player player) {
                 this.returnLocation = returnLocation;
                 this.entity = entity;
+                this.toLookAt = player;
             }
 
             public void run() {
-                this.entity.move(returnLocation);
+                // Walk the NPC back to its original location.
+                this.entity.move(this.returnLocation);
+
+                // Turn back to the player to correspond with te walking.
+                entity.lookAt(this.toLookAt);
             }
         }
 
-        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new ReturnNPC(npcLocation, behaviorEntity), wait + 20L);
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new ReturnNPC(npcLocation, behaviorEntity, inPlayer), wait + 20L);
 
-
+        // Ran once the NPC is actually back near the player.
         class ReturnItem implements Runnable {
             private final Player player;
             private final ItemStack item;
@@ -175,29 +174,36 @@ public class BlacksmithInteractBehavior extends InteractBehavior {
 
             public void run() {
                 if (this.player.getInventory().getItem(this.attemptSlot) == null && this.player.isOnline()) {
+                    // Called if the player still has a free slot where the item originated from.
                     this.player.getInventory().setItem(this.attemptSlot, this.item);
                 } else if (this.player.getInventory().firstEmpty() != -1 && this.player.isOnline()) {
+                    // Called if the player has a free space, but it's not the origin slot.
                     this.player.getInventory().setItem(this.player.getInventory().firstEmpty(), this.item);
                 } else {
-                    Item droppedItem = npc.getLocation().getWorld().dropItem(npc.getLocation(), this.item);
-                    droppedItem.setVelocity(new Vector(0, 0, 0));
+                    // Called only if the player isn't online or has a full inventory. Avoid if possible to prevent item destruction.
+                    npc.getLocation().getWorld().dropItem(npc.getLocation(), this.item);
                 }
 
+                // Display anvil-names, if it has one.
                 if (item.getItemMeta().hasDisplayName()) {
                     this.player.sendMessage(ChatColor.AQUA + this.npc.getDisplayName() + ChatColor.GREEN + " repaired " + ChatColor.YELLOW + item.getItemMeta().getDisplayName() + ChatColor.GREEN + ".");
                 } else {
                     this.player.sendMessage(ChatColor.AQUA + this.npc.getDisplayName() + ChatColor.GREEN + " repaired your " + ChatColor.YELLOW + item.getType().name().replace("_", " ").toLowerCase() + ChatColor.GREEN + ".");
                 }
 
-                ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
+                // Put the blacksmith's default item back in his hand.
+                ItemStack axe = new ItemStack(defaultItem);
                 axe.addEnchantment(Enchantment.SILK_TOUCH, 1);
                 npc.setItemInHand(axe);
 
+                // Grab the RemoteEntity to perform logic operations.
                 RemoteEntity entity = RemoteEntities.getRemoteEntityFromEntity(this.npc);
+
+                // Restore normal livelihood into the blacksmith.
                 DesireLookAtNearest desireLookAtNearest = entity.getMind().getMovementDesire(DesireLookAtNearest.class);
                 DesireLookRandomly desireLookRandomly = entity.getMind().getMovementDesire(DesireLookRandomly.class);
 
-                desireLookAtNearest.startExecuting();
+                desireLookAtNearest.setLookPossibility(1.0F);
                 desireLookRandomly.startExecuting();
             }
         }
@@ -205,6 +211,7 @@ public class BlacksmithInteractBehavior extends InteractBehavior {
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new ReturnItem(inPlayer, repairItem, npc, indexOfPreviouslyHeldItem), (wait * 2) + 20L);
     }
 
+    // Grab the nearest location of a block with a certain material/type.
     private Location closestLocation(Location origin, Material type) {
         World world = origin.getWorld();
 

8c298c9edef021c9c1acdb0c8400909c8aedb896
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-09-22 18:02:50 -0700

    Bump version number and clean up code. Also add saving on disable.

--- src/main/java/com/bitlimit/NPCs/NPCs.java
+++ src/main/java/com/bitlimit/NPCs/NPCs.java
@@ -22,33 +22,12 @@ public class NPCs extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         try {
-//            this.manager = RemoteEntities.createManager(this);
-//            this.manager.setEntitySerializer(new YMLSerializer(this));
-//            this.manager.saveEntities();
-//
-//            this.manager.setSaveOnDisable(true);
-//            this.manager.loadEntities();
-
             this.manager = RemoteEntities.createManager(this);
 
-            //First we register the serializer that should be used
-            //In this case we use the YML serializer, but you can also use your own as well as the json serializer
             this.manager.setEntitySerializer(new YMLSerializer(this));
-            //To load the entities we saved, you can just do this:
+            this.manager.setSaveOnDisable(true);
             this.manager.loadEntities();
-
-            //If we want to save all current entities, it's pretty easy:
-//            this.manager.saveEntities();
-//
-//            //Some serializers allow single entities to be saved, i.e. the YML serializer
-//            //But first, we need to create an entitiy.
-//            RemoteEntity entity = this.manager.createNamedEntity(RemoteEntityType.Human, Bukkit.getWorld("world").getSpawnLocation(), "Smith");
-//            //Now we can just call the save method. Keep in mind that it will not work when the serializer doesn't support single entity serialization
-//            entity.save();
-
-                        //Now all the entities should be back like you never removed them
         } catch (Exception e) {
-            this.manager = null;
             e.printStackTrace();
         }
 
@@ -78,7 +57,6 @@ public class NPCs extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
-
         if (!(event.getEntity() instanceof LivingEntity)) {
             return;
         }
